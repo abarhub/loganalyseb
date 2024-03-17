ad58e55e4f04b401549c0606d10e903487444e70
abarhub <abarhub@gmail.com>
2024-03-17 09:13:47 +0100

    commit initial

---
+++ src/main/java/org/loganalyseb/loganalyseb/LoganalysebApplication.java
@@ -0,0 +1,13 @@
+package org.loganalyseb.loganalyseb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoganalysebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LoganalysebApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/loganalyseb/loganalyseb/LoganalysebApplicationTests.java
@@ -0,0 +1,13 @@
+package org.loganalyseb.loganalyseb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoganalysebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
